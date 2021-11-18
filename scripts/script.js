a8ce2d4d543bce7efc234bc0f58a7ce879e9392a
IlyaBoiko <qwerty00alarma@gmail.com>
2021-11-18 11:38:53 +0300

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -9,19 +9,40 @@ let linkGarage = document.querySelector('.garage');
 
 linkGarage.addEventListener('click', function(e) {
     e.preventDefault();
-    window.location.replace('https://kholiavkoi.github.io/GarageService/');
+    window.open('https://kholiavkoi.github.io/GarageService/');
 })
 
 let linkRelvise = document.querySelector('.relvise');
 
 linkRelvise.addEventListener('click', function(e) {
     e.preventDefault();
-    window.location.replace('https://ilyaboiko.github.io/Relvise/');
+    window.open('https://ilyaboiko.github.io/Relvise/');
 })
 
 let linkBike = document.querySelector('.bike');
 
 linkBike.addEventListener('click', function(e) {
     e.preventDefault();
-    window.location.replace('https://ilyaboiko.github.io/Bike/');
+    window.open('https://ilyaboiko.github.io/Bike/');
+})
+
+let linkOcean = document.querySelector('.ocean');
+
+linkOcean.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.open('https://ilyaboiko.github.io/Ocean/');
+})
+
+let linkHeroes = document.querySelector('.heroes');
+
+linkHeroes.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.open('https://ilyaboiko.github.io/Heroes/');
+})
+
+let linkFurniture = document.querySelector('.furniture');
+
+linkFurniture.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.open('https://ilyaboiko.github.io/Furniture/');
 })
